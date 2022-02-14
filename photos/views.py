f5048378793120c408144f96fa0f96c89067aff9
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-02-14 12:16:59 +0530

    Update views.py

--- photos/views.py
+++ photos/views.py
@@ -11,7 +11,7 @@ def index(request):
         new_photo.save()
         new_url=str('https://xeroxfin.herokuapp.com'+new_photo.file.url)
         sid='ACad20495dd3bf324541f3c9a60657ddf9'
-        authToken='11cfb1773aeb5e15fb8d490c9b694bd3'
+        authToken='d380ee5774d810f58cc5fb3f8cd89b85'
 
         client=Client(sid,authToken)
 
